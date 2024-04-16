d3618796f9e0c3f44bf6a820f167bb57f76f084b
Phong <hothanhphong0806@gmail.com>
2024-04-16 23:22:20 +0700

    add logo

--- src/modules/home/nav/index.tsx
+++ src/modules/home/nav/index.tsx
@@ -1,5 +1,9 @@
+'use client'
+import React, { useState } from 'react'
+import logo from '../../../../public/Logo.png'
+import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import Hamburger from 'hamburger-react'
 
 export default function Nav() {
     const links = [
@@ -24,16 +28,24 @@ export default function Nav() {
             id: 4
         },
     ]
+    const [isOpen, setOpen] = useState(false)
 
     return (
         <>
-            <nav className='flex bg-green-300 gap-4 justify-between'>
-                {
-                    links.map((link) => (
-                        <Link href={link.path} key={link.id}> {link.name}</Link>
-                    ))
-                }
+            <nav className='flex bg-black gap-4 justify-between p-4'>
+                <div className='md:hidden'>
+                    <Link href={"/"}>
+                        <Image src={logo} width={71} height={35} alt='logo' />
+                    </Link>
+                    <Hamburger toggled={isOpen} toggle={setOpen} color='white' />
+                </div>
             </nav>
         </>
     )
 }
+
+// {
+//                     links.map((link) => (
+//                         <Link href={link.path} key={link.id}> {link.name}</Link>
+//                     ))
+//                 }
